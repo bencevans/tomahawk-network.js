0aa71014c4bc8d4aec5fd03b72eb1e3145a16962
Ben Evans <ben@bensbit.co.uk>
2013-12-09 08:00:39 +0000

    Make announcements on lib/discover optional

--- examples/discover.js
+++ examples/discover.js
@@ -3,8 +3,9 @@
 var uuid = require('uuid');
 
 var discover = require('../lib/discover')({
-  nodeId: uuid.v4(), // An ID used to represent your Tomahawk Network Instance
-  port: 50210        // The TCP server port
+  // If a nodeId and port are provided, the discover library will automatically announce aswell as discover
+  nodeId: uuid.v4(), // (optional) An ID used to represent your Tomahawk Network Instance
+  port: 50210        // (optional) The TCP server port to be advertised
 } /* (optional) deviceCallback */);
 
 discover.on('device', function(device) {
